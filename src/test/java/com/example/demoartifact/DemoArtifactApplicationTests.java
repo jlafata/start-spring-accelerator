b16a31731caf8250d1ba0b8a92b0a91392f55191
John LaFata <jlafata@vmware.com>
2022-06-07 11:26:44 -0400

    first commit

---
+++ src/test/java/com/example/demoartifact/DemoArtifactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoartifact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoArtifactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
